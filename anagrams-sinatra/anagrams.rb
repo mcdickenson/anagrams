306edabc55bcf46eea5b7baf59df66ef79cd135a
Matt Dickenson <mcdickenson@gmail.com>
2016-03-27 10:45:28 -0600

    Add DELETE endpoints

--- anagrams-sinatra/anagrams.rb
+++ anagrams-sinatra/anagrams.rb
@@ -1,9 +1,3 @@
-# - `POST /words.json`: Takes a JSON array of English-language words and adds them to the corpus (data store).
-# - `GET /anagrams/:word.json`:
-#   - Returns a JSON array of English-language words that are anagrams of the word passed in the URL.
-#   - This endpoint should support an optional query param that indicates the maximum number of results to return.
-# - `DELETE /words/:word.json`: Deletes a single word from the data store.
-# - `DELETE /words.json`: Deletes all contents of the data store.
 require 'sinatra'
 require 'redis'
 require 'hiredis'
@@ -19,7 +13,8 @@ helpers do
   end
 end
 
-post '/words.json' do
+# Add words to corpus
+post '/words.:format?' do
   request.body.rewind
   data = JSON.parse(request.body.read)
 
@@ -35,6 +30,7 @@ post '/words.json' do
   status 201
 end
 
+# Get anagrams for a word
 get '/anagrams/:word.:format?' do
   word = params['word']
   anagrams = if params['limit']
@@ -47,4 +43,16 @@ get '/anagrams/:word.:format?' do
   anagrams ||= []
 
   { anagrams: anagrams }.to_json
+end
+
+# Delete all words
+delete '/words.:format?' do
+  redis.flushall
+  status 204
+end
+
+# Delete single word
+delete '/words/:word.:format?' do
+  word = params['word']
+  redis.srem(key(word), word)
 end
